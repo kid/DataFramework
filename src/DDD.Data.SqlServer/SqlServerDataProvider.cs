0b4d88b0fafb38d585ae107b87ab842de8d3f3d1
Arnaud Rebts <arnaud.rebts@gmail.com>
2010-12-14 17:41:29 +0100

    Adds DDD.Data.SqlServer project.

---
+++ src/DDD.Data.SqlServer/SqlServerDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DDD.Data.Storage;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DDD.Data.SqlServer
+{
+    public class SqlServerDataProvider : IDataProvider
+    {
+        private readonly string connectionString;
+
+        public SqlServerDataProvider(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
