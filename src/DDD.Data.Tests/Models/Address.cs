32b428c9a61f34017a330b1ef0aaff0dca085654
Arnaud Rebts <arnaud.rebts@gmail.com>
2010-12-14 17:33:39 +0100

    Complete the Address test model.

--- src/DDD.Data.Tests/Models/Address.cs
+++ src/DDD.Data.Tests/Models/Address.cs
@@ -1,5 +1,5 @@
-﻿
-using DDD.Data.MetaData;
+﻿using DDD.Data.MetaData;
+
 namespace DDD.Data.Tests.Models
 {
     [Table("Addresses")]
@@ -7,5 +7,9 @@ namespace DDD.Data.Tests.Models
     {
         [PrimaryKey]
         public int? Id { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
     }
 }
